9a1a1d28b127c26279b3ce526a1ef0c46a5db4db
ghostttt420 <prvda.xo@gmail.com>
2026-01-02 23:15:18 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Calculator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
